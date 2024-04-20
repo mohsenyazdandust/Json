cb29e4d7f8328f066abdf2e85fea84352ce07f54
Mohsen Yazdandust <m.yazdandust1403@gmail.com>
2024-04-20 18:30:00 +0330

    Add 3 Human Objects to list

--- Json/Program.cs
+++ Json/Program.cs
@@ -1,10 +1,49 @@
 ﻿namespace Json
 {
-    internal class Program
+    public struct Human
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public int Age { get; set; }
+        public char Gender { get; set; }
+    }
+
+    internal class Program
+    {        
+        public static List<Human> humans = new List<Human>();
+
+        public static void CreateHumans()
+        {
+            humans.Append<Human>(new Human { 
+                FirstName="Mohsen",
+                LastName="Yazdandust",
+                NickName="themohsen",
+                Age=23,
+                Gender='M',
+            });
+
+            humans.Append<Human>(new Human
+            {
+                FirstName = "Abolfazl",
+                LastName = "Navehosseini",
+                NickName = "Abolo",
+                Age = 20,
+                Gender = 'M',
+            });
+
+            humans.Append<Human>(new Human
+            {
+                FirstName = "Javad",
+                LastName = "Bayrami",
+                NickName = "Javadi",
+                Age = 23,
+                Gender = 'M',
+            });
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CreateHumans();
         }
     }
 }
